ae3441c084aa87fb758d9740dca3271549f38b4b
Le Nguyen Phuong Thai <thailnp133@gmail.com>
2023-01-16 09:25:22 +0700

    generate function

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React, { useEffect } from "react";
 
 
 function App() {
-  
+  // Generator function
+  useEffect(()=>{
+    function* demoGenerator(){
+      console.log("running");
+      let id=1;
+      while(true){
+        yield id;
+        id++;
+      }
+    }
+    const gen = demoGenerator();
+    console.log(gen.next());
+  },[])
   return (
     <div>
       
